2ea399d1eab0ab3e89da78a382bc820545278732
Netzvamp <robertlieback@zetabyte.de>
2020-12-09 23:22:39 +0100

    Fixed readthedocs rendering

--- docs/conf.py
+++ docs/conf.py
@@ -13,10 +13,9 @@ import os
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath("."))
-
+import os
+import sys
+sys.path.insert(0, os.path.abspath(".."))
 
 # -- Project information -----------------------------------------------------
 
